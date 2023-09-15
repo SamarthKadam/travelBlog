bf5c816a21b7ba85f1f0965ddba779aa361d2a04
PanariRasul <panarirasul@gmail.com>
2023-09-16 01:11:59 +0530

    add js for add blog button

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-$(document).ready(function () {
-    $("#toggleIcon").click(function() {
-        $("ul").toggleClass("show")
-    });
-});
+// $(document).ready(function () {
+//     $("#toggleIcon").click(function() {
+//         $("ul").toggleClass("show")
+//     });
+// });
+
+
+const blogBox = document.querySelector(".blog_section_box");
+console.log(blogBox);
+
+const save = () => {
+    const blogData = {
+        id: `${Date.now()}`,
+        imageUrl: document.getElementById("imageurl").value,
+        date: document.getElementById("date").value,
+        placeName: document.getElementById("placename").value,
+        discription: document.getElementById("description").value
+    };
+    console.log(blogData);
+};
