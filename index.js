509acd13f3a459273b8fe8dad2d2d6b8c6552d7e
PanariRasul <panarirasul@gmail.com>
2023-09-19 21:46:52 +0530

    Connect MongoDB to contact form

--- index.js
+++ index.js
@@ -1,38 +1,240 @@
-const bodyParser = require("body-parser");
-var express = require("express");
-var mongoose = require("mongoose");
-const { log } = require("console");
+// // first import install libery 
+
+// var express = require("express");
+// var bodyParse = require("body-parser");
+// var mongoose = require("mongoose");
 
 
+// //create app
+
+// const app = express()
+
+// app.use(bodyParse.json())
+// app.use(express.static('public'))
+// app.use(bodyParse.urlencoded({
+//     extended: true
+// }))
+
+// // conect database
+
+
+// mongoose.connect('mongodb://0.0.0.0:27017/mydb', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+
+// var db = mongoose.connection;
+
+// // check connect
+
+// db.on('error', () => console.log("error in connecting database"));
+// db.once('open', () => console.log("Connected to Database"));
+
+
+// app.get("/", (req, res) => {
+
+//     res.set({
+//         "Allow-access-Allow-Origin": '*'
+//     })
+
+//     return res.redirect('index.html');
+
+// }).listen(3001);
+
+
+// app.post("/contact", (request,response) =>{
+//     try {
+//         //get data from index.html
+
+//         const firstname = request.body.firstname;
+//         const lastname = request.body.lastname;
+//         const email = request.body.email;
+//         const subject = request.body.subject;
+//         const    discription = request.body.discription;
+
+//         console.log( `${firstname}, ${lastname},${email},${subject} & ${discription}`);
+//     }catch(error){
+//         console.log("invalid information");
+//     }
+
+
+
+
+
+// })
+
+
+
+
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
 
 const app = express()
 
 app.use(bodyParser.json())
-app.use(express.static("public"))
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }))
 
-
-
-// connect database
-
-mongoose.connect("mongodb://localhost:27017/mydb", {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-
-
+mongoose.connect('mongodb://0.0.0.0:27017/mydb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 var db = mongoose.connection;
 
-// check connection
+db.on('error', () => console.log("Error in Connecting to Database"));
+db.once('open', () => console.log("Connected to Database"));
+app.post("/contact", (request, res) => {
+    var firstname = request.body.firstname;
+    var lastname = request.body.lastname;
+    var email = request.body.email;
+    var subject = request.body.subject;
+    var discription = request.body.place_desc;
 
-db.on("error", () => console.log("error in connection database"));
-db.once("open", () => console.log("connected to database"));
+    
 
+    var data = {
+        "firstname":firstname,
+        "lastname":lastname,
+        "email":email,
+        "subject":subject,
+        "discription":discription
+    }
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) throw err;
+        console.log("Record Inserted Successfully");
+    });
+    return res.redirect('submit.html');
 
-
-// checking page
-app.get("/", (req, res) => {
-    return res.redirect("index.html");
 })
+
+app.get("/", (req, res) => {
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+
+}).listen(3000);
+
+console.log("Listening on Port 3000");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const bodyParser = require("body-parser");
+// var express = require("express");
+// var mongoose = require("mongoose");
+// const { log } = require("console");
+// const { request } = require("http");
+
+
+
+// const app = express()
+
+// app.use(bodyParser.json())
+// app.use(express.static("public"))
+// app.use(bodyParser.urlencoded({
+//     extended:true
+// }))
+
+
+
+// // connect database
+
+// mongoose.connect("mongodb://0.0.0.0:27017/mydb", {
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// })
+
+
+// var db = mongoose.connection;
+
+// // check connection
+
+// db.on("error", () => console.log("error in connection database"));
+// db.once("open", () => console.log("connected to database"));
+
+
+
+// // checking page
+// app.get("/", (req, res) => {
+//     res.set({
+//         "Allow-access-Allow-Origin":'*'
+//     })
+
+//     return res.redirect("index.html");
+// }).listen(3000);
+
+
+
+
+// app.post("/contact", (request,response) =>{
+//     try {
+//         //get data from index.html
+
+//         var firstname = request.body.firstname;
+//         var lastname = request.body.lastname;
+//         var email = request.body.email;
+//         var subject = request.body.subject;
+//         var discription = request.body.place_desc;
+
+//         console.log( `${firstname}, ${lastname},${email},${subject} & ${discription}`);
+//     }catch(error){
+//         console.log("invalid information");
+//     }
+
+
+//     // create a object
+
+//     var data = {
+//         "firstname":firstname,
+//         "lastname":lastname,
+//         "email":email,
+//         "subject":subject,
+//         "discription":discription
+//     }
+
+
+//     db.collection('users').insertOne(data, (err, collection) => {
+//         if (err) throw err;
+//         console.log("Record insert Successfully");
+//     });
+
+
+//     return res.redirect("submit.html");
+
+    
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // tQg505g9aTPeRpsP
+
+
+// // mongodb+srv://rasulpanari01:<password>@cluster0.k8ceudw.mongodb.net/
